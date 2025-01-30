02f679610aa156241bc45f54a3bf9fb349a4fd48
Oluwapelumi <pelekadeba@gmail.com>
2025-01-30 05:47:26 +0100

    contextsave

--- main.py
+++ main.py
@@ -98,4 +98,5 @@ def handle_disconnect():
     print("Client disconnected!")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    with app.app_context():
+        socketio.run(app, debug=True, host="0.0.0.0", port=5000)
